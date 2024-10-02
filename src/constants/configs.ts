26d583fd2400ecd7c99998091e1e02114fbae0c3
d4rkriverr <hamza.gharbi.a60@gmail.com>
2024-10-02 09:52:56 +0100

    s

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,6 +1,6 @@
 const configs = {
     TOKEN_NAME: 'authToken',
-    API_URI: 'http://localhost:8080'
-    // API_URI: 'http://emnaservices.online',
+    // API_URI: 'http://localhost:8080'
+    API_URI: 'http://emnaservices.online',
 }
 export default configs;
